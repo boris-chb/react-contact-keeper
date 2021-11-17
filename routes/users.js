7c0ce6d52660a3402966f784c4aa8fbecbaa3c60
Boris <47457848+boris-chb@users.noreply.github.com>
2021-11-17 13:13:07 +0000

    Added MongoDB. Implemented User model + validation

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,29 @@
 const express = require("express");
-
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+
+// Models
+const User = require("../models/User");
 
 // @route   POST    api/users
 // @desc    Register a new User
 // @access  Public
-router.post("/", (req, res) => {
-  res.send("Registering an User");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please enter a name").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Please enter a password (6+ characters)").isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("Success");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// Connect DB
+connectDB();
+
+// Init Middleware
+app.use(
+  express.json({
+    extended: false,
+  })
+);
 
 app.get("/", (req, res) =>
   res.json({
